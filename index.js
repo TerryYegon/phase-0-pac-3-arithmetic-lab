ec42464ec748f67a5f310a51601a19054bd20077
Terry Yegon <yegonterry618@gmail.com>
2025-05-30 15:10:35 +0300

    adding solution to index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+1 + 80;
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a ,b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+   return a * b;
+}
+function divide(a, b) {
+    return a / b
+}
+
+// Increments n and returns the result
+function increment(n) {
+  return n + 1;
+}
+
+// Decrements n and returns the result
+function decrement(n) {
+  return n - 1;
+}
+
+// Parses n as an integer, assumes base 10, and returns the parsed integer
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+// Parses n as a floating point number and returns the parsed number
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
